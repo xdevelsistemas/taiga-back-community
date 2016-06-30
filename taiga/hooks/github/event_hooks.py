7b42b03aebdbf367e12e7e2f49cd064453d9a4d3
alvarollmenezes <alvarollmenezes@gmail.com>
2016-06-30 12:04:43 -0300

    Fix

--- taiga/hooks/github/event_hooks.py
+++ taiga/hooks/github/event_hooks.py
@@ -158,7 +158,7 @@ class IssuesEventHook(BaseEventHook):
     def _process_labeled(self, user, github_url):
         issues = Issue.objects.filter(external_reference=["github", github_url])
         
-        labels = [x.name for x in list(self.payload.get('labels', []))]
+        labels = [x['name'] for x in list(self.payload.get('labels', []))]
 
         # (Testar) pesquisar os tipos dos issues pelo nome dos labels e trazer o primeiro ordenado pelo order crescente 
         issueType = IssueType.objects.filter(project=self.project, name__in=labels).order_by('order').first()
